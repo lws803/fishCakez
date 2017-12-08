1f24186a6a2b91c49db62e65738db45d94fbb117
AUTUMNSIGIL\TTG <omnikron96@gmail.com>
2017-12-08 17:59:28 +0800

    Added HP GUI and pause menu button.

---
+++ Shift/Assets/HPBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPBar : MonoBehaviour {
+
+	public float currentHP;
+	public float maxHP;
+	public RectTransform redBar;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float widthSize = (currentHP / maxHP) * 128;
+		redBar.sizeDelta = new Vector2 (widthSize, 38.4);
+	}
+
+	public void damaged(float amount){
+		currentHP -= amount;
+	}
+
+	public void heal(float amount){
+		currentHP += amount;
+	}
+}

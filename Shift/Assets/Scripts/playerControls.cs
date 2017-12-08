3bf48467959c0cca3209a342c093d24fbeb6c89e
lws803 <lws803@gmail.com>
2017-12-08 16:50:21 +0800

    Added movement scripts

--- Shift/Assets/Scripts/playerControls.cs
+++ Shift/Assets/Scripts/playerControls.cs
@@ -5,52 +5,60 @@ using UnityEngine;
 public class playerControls : MonoBehaviour {
 
 	public float speed;				//Floating point variable to store the player's movement speed.
+	public float gridSize;
+
+	public LayerMask mask;
 
 	Vector3 pos;
-	Transform tr;
+	//Transform tr;
+	//bool collided = false; 
 
 	// Use this for initialization
 	void Start()
 	{
 		pos = transform.position;
-		tr = transform;
+		//tr = transform;
 	}
 
 	void FixedUpdate()
 	{
-		if (Input.GetKey(KeyCode.D) && tr.position == pos)
-		{
-			pos += Vector3.right;
+		//====RayCasts====//
+		RaycastHit2D hitup = Physics2D.Raycast(transform.position, Vector2.up, gridSize, mask.value);
+		RaycastHit2D hitdown = Physics2D.Raycast(transform.position, Vector2.down, gridSize, mask.value);
+		RaycastHit2D hitright = Physics2D.Raycast(transform.position, Vector2.right, gridSize, mask.value);
+		RaycastHit2D hitleft = Physics2D.Raycast(transform.position, Vector2.left, gridSize, mask.value);
+		//==Inputs==//
+		if (Input.GetKey(KeyCode.A) && transform.position == pos && hitleft.collider  == null)
+		{           //(-1,0)
+			pos += Vector3.left * gridSize;// Add -1 to pos.x
 		}
-		else if (Input.GetKey(KeyCode.A) && tr.position == pos)
-		{
-			pos += Vector3.left;
+		if (Input.GetKey(KeyCode.D) && transform.position == pos && hitright.collider == null)
+		{           //(1,0)
+			pos += Vector3.right * gridSize;// Add 1 to pos.x
 		}
-		else if (Input.GetKey(KeyCode.W) && tr.position == pos)
-		{
-			pos += Vector3.up;
+		if (Input.GetKey(KeyCode.W) && transform.position == pos && hitup.collider    == null)
+		{           //(0,1)
+			pos += Vector3.up * gridSize; // Add 1 to pos.y
 		}
-		else if (Input.GetKey(KeyCode.S) && tr.position == pos)
-		{
-			pos += Vector3.down;
+		if (Input.GetKey(KeyCode.S) && transform.position == pos && hitdown.collider  == null)
+		{           //(0,-1)
+			pos += Vector3.down * gridSize;// Add -1 to pos.y
 		}
-
-		transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
-
-
+		//The Current Position = Move To (the current position to the new position by the speed * Time.DeltaTime)
+		transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);    // Move there
 	}
 
 	//OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
-	void OnTriggerEnter2D(Collider2D other) 
+	void OnCollisionEnter2D(Collision2D other) 
 	{
+		print ("Collided");
 		//Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
 		if (other.gameObject.CompareTag ("PickUp")) 
 		{
 			// Dump in some collder code here
+		}else{
+			//collided = true;
 		}
-
-
 	}
-
 
 }

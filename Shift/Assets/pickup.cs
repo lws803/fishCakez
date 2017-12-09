54d80f76a13a5f2c86c541a982cfa0b0b42bbae1
AUTUMNSIGIL\TTG <omnikron96@gmail.com>
2017-12-09 08:06:53 +0800

    hjghjghj

---
+++ Shift/Assets/pickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour {
+	public GameObject floating;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D collider) {
+		Destroy (floating);
+	}
+}
